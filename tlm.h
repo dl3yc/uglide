b9fc4ce1baa1ac96a3710c61dfd5baafa8db4124
Sebastian Weiss <dl3yc@darc.de>
2017-05-07 16:49:53 +0200

    remove superfluous function

--- tlm.h
+++ tlm.h
@@ -4,14 +4,10 @@
 #include <inttypes.h>
 #include "main.h"
 
-/* because tick is slower when using DominoEX,
- * use less ticks for blipping
- */
 #define	BLIP_FACTOR	5
 
 void tx_blips(uint8_t reset);
 void init_tx_buffer(void);
 void prepare_tx_buffer(void);
-void tlm_init(void);
 
 #endif
